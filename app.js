f7d5eca3d22ba2e3df218b38aa068f472e337d3f
George-Gyang <georgegyang3@gmail>
2023-01-18 15:48:11 +0100

    adding footer

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // alert("i am connected!!!")
+const year = document.getElementById("year")
+
+const d = new Date()
+    const getYear =d.getFullYear()
+    year.innerHTML = getYear;
+
+    // AOS INITIATOR
+    <script>
+  AOS.init();
+</script>
