ae5625945506df38f88664b1d31542ed2bb1e8b1
George-Gyang <georgegyang3@gmail>
2023-01-18 20:37:20 +0100

    adding scrolling functionality

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-// alert("i am connected!!!")
+AOS.init();
+
+
+// (function () {
+//     // AOS.init();
+//     window.addEventListener('load', () => {
+//         AOS.init({
+//           duration: 1000,
+//           easing: 'ease-in-out',
+//           once: true,
+//           mirror: false
+//         })
+//       });
+// })()
+
+
+//  alert("i am connected!!!")
 const year = document.getElementById("year")
 
 const d = new Date()
-    const getYear =d.getFullYear()
-    year.innerHTML = getYear;
+const getYear = d.getFullYear()
+year.innerHTML = getYear;
 
-    // AOS INITIATOR
-    <script>
-  AOS.init();
-</script>
+// AOS INITIATOR
+// <script>
+// AOS.init();
+{/* </script> */ }
+
